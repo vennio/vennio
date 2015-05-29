c808cbd5dac6fbd580d823a20c722f3ae0a06fbb
brantchoate <brantchoate@gmail.com>
2015-05-28 20:38:01 -0700

    added sass compile, uglify, hint, concat, and dist folder creation on save and when the default gulp task is running

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,58 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
 var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
+var less = require('gulp-sass');
 
-gulp.task('js', function () {
-  gulp.src('client/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(uglify())
+var paths = {
+  scss: 'client/css/*.scss'
+};
+
+gulp.task('js', function() {
+  gulp.src([
+    'client/bower_components/d3/d3.js',
+    'client/js/*.js'
+    ])
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+    
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('jshint', function() {
+  return gulp.src('client/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+})
+
+gulp.task('html', function() {
+  gulp.src('client/*.html').pipe(gulp.dest('build'));
+});
+
+gulp.task('img', function() {
+  gulp.src('client/img/**').pipe(gulp.dest('build/img'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src([
+    'client/bower_components/skeleton/normalize.css',
+    'client/bower_components/skeleton/skeleton.css',
+    'client/font/stylesheet.css',
+    "client/css/*.scss"
+  ])
+  .pipe(sass())
+  .pipe(gulp.dest("build/css"))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('bsync', function() {
   var files = [
     'client/*.html',
     'client/css/*.css'
@@ -21,10 +60,20 @@ gulp.task('browser-sync', function() {
 
   browserSync.init(files, {
     server: {
-      baseDir: './client'
+      baseDir: './build'
     }
   })
+
 });
 
-gulp.task('build', ['js']);
-gulp.task('default', ['browserSync']);
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+})
+
+gulp.task('default', ['sass', 'bsync', 'js', 'html', 'img'], function() {
+
+  gulp.watch(['client/js/*.js'], ['jshint','js']);
+  gulp.watch(['client/css/**/*.scss'], ['sass']);
+  gulp.watch(['*.html'], ['html','bs-reload']);
+
+});
