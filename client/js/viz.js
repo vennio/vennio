984c4d0eac3c65179a85d0d749abefa9e7ccc210
brantchoate <brantchoate@gmail.com>
2015-06-03 12:06:25 -0700

    some minor style changes

--- client/js/viz.js
+++ client/js/viz.js
@@ -1,81 +1,70 @@
- var updateData = function(input){
+ var updateData = function(input, selector){
     var categories = input.categories;
     var dollars = input.salaries;
     var colors = input.colors;
 
-    var width = 900;
+    var width = $('.barchart').width();
     var height = 750;
     var offset = 19;
 
-    var grid = d3.range(25).map(function(i){
-      return {'x1':0,'y1':0,'x2':0,'y2':480};
-    });
-
-    var tickVals = grid.map(function(d,i){
-      if(i>0){ return i*10; }
-      else if(i===0){ return "100";}
-    });
-
-    // 
     var xscale = d3.scale.linear()
-            .domain([70,130])
-            .range([0,722]);
-
+      .domain([0,130])
+      .range([0,width]);
 
     var yscale = d3.scale.linear()
-            .domain([0,categories.length])
-            // .range([0,480]); // Original
-            .range([0,70*categories.length]);
+      .domain([0,categories.length])
+      // .range([0,480]); // Original
+      .range([0,70*categories.length]);
 
     var colorScale = d3.scale.quantize()
-            .domain([0,categories.length])
-            .range(colors);
+      .domain([0,categories.length])
+      .range(colors);
 
     // Remove previous svg if exists
     d3.select('svg').remove();
 
-    var canvas = d3.select('.wrapper')
-            .append('svg')
-            .attr({'width':width,'height':height});
+    var canvas = d3.select(selector)
+      .append('svg')
+      .attr({'width':width,'height':height});
 
     var yAxis = d3.svg.axis()
-        .orient('right')
-        .scale(yscale)
-        .tickSize(0)
-        .tickFormat(function(d,i){ return categories[i]; })
+      .orient('right')
+      .scale(yscale)
+      .tickSize(0)
+      .tickFormat(function(d,i){ return categories[i]; })
 
     var chart = canvas.append('g')
-              .attr("transform", "translate(0,0)")
-              .attr('id','bars')
-              .selectAll('rect')
-              .data(dollars)
-              .enter()
-              .append('rect')
-              .attr('height',65)
-              .attr({'x':0,'y':function(d,i){ return yscale(i) ; }})
-              .style('fill',function(d,i){ return colorScale(i); })
-              .attr('width',function(d){ return 0; });
+      .attr("transform", "translate(0,0)")
+      .attr('class','bars')
+      .selectAll('rect')
+      .data(dollars)
+      .enter()
+      .append('rect')
+      .attr('height',65)
+      .attr({'x':0,'y':function(d,i){ return yscale(i) ; }})
+      .style('fill',function(d,i){ return colorScale(i); })
+      .attr('width',function(d){ return 0; });
 
     var y_xis = canvas.append('g')
-              .attr("transform", "translate(10,-40)")
-              .attr('id','yaxis')
-              .call(yAxis);
+      .attr("transform", "translate(10,-40)")
+      .attr('id','yaxis')
+      .call(yAxis);
 
 
     var transit = d3.select("svg").selectAll("rect")
-                .data(dollars)
-                .transition()
-                .duration(1000) 
-                .attr("width", function(d) {return xscale(d); });
+      .data(dollars)
+      .transition()
+      .duration(1000) 
+      .attr("width", function(d) {return xscale(d); });
 
-    var transitext = d3.select('#bars')
-              .selectAll('text')
-              .data(dollars)
-              .enter()
-              .append('text')
-              .attr('text-anchor', 'end')
-              .attr({'x':function(d) {return xscale(d) - 10; },'y':function(d,i){ return yscale(i)+42; }})
-              .text(function(d){ return "$" + d+"k"; }).style({'fill':'#fff','font-size':'30px'});
+    var transitext = d3.select('.bars')
+      .selectAll('text')
+      .data(dollars)
+      .enter()
+      .append('text')
+      .attr('text-anchor', 'end')
+      .attr({'x':function(d) {return xscale(d) - 10; },'y':function(d,i){ return yscale(i)+42; }})
+      .text(function(d){ return "$" + d+"k"; }).style({'fill':'#fff','font-size':'30px'});
   };
 
   // Skills 
@@ -111,9 +100,9 @@
     return input;
   };
 
-  updateData(generateInput());
+	updateData(generateInput(), '.wrapper1');
+
   $("#update").click(function(){
-    updateData(generateInput());
   });
 
 
