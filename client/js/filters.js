5de7f77a5fb31300e01425e0600a639b8b501c2a
brantchoate <brantchoate@gmail.com>
2015-06-05 11:50:47 -0700

    added off click UI functionality + more modular approach to the code for reuse

--- client/js/filters.js
+++ client/js/filters.js
@@ -6,9 +6,7 @@ $(function(){
   $(document).mouseup(function(e) {
     var container = $('.filter-list');
 
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
       container.hide();
     }
   });
