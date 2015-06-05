8db2f4086dbe825d3baee34f520f1181ca90acb2
brantchoate <brantchoate@gmail.com>
2015-06-05 11:30:17 -0700

    added first pass at filters UI components

---
+++ client/js/filters.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.filter-button').on('click', function(e) {
+    $(this).next().toggle();
+  });
+
+  $(document).mouseup(function(e) {
+    var container = $('.filter-list');
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.hide();
+    }
+  });
+
+});
