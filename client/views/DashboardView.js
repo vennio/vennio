705e0c6e0cc97e1ae90d3778f6c56fece81fe7e0
Irfan Baqui <irfan.baqui@gmail.com>
2015-06-13 14:35:24 -0700

    Implement event handling for navbar

--- client/views/DashboardView.js
+++ client/views/DashboardView.js
@@ -7,15 +7,24 @@ var DashboardView = Backbone.View.extend({
 
 	initialize: function(params) {
     this.dashboardType = params.type;
-    this.group = 'Skill';
-    if (this.dashboardType === 'SalaryJobBySkill')
-      this.initializeModels();
+    
+    if (this.dashboardType === 'SalaryJobBySkill') {
+      this.group = 'Skill';
+      this.jobSalaryEndpoint = 'SalaryJobBySkill';
+      this.companyEndpoint = 'CompanyBySkill';
+    } else {
+      this.group = 'Location';
+      this.jobSalaryEndpoint = 'SalaryJobByLocation';
+      this.companyEndpoint = 'CompanyByLocation';
+    }
+      
+    this.initializeModels();
 	},
 
   initializeModels: function() {
 
-    var companyModel = new WrapperModel({endpoint: 'CompanyBySkill'});
-    var jobSalaryModel = new WrapperModel({endpoint: 'SalaryJobBySkill'});
+    var companyModel = new WrapperModel({endpoint: this.companyEndpoint});
+    var jobSalaryModel = new WrapperModel({endpoint: this.jobSalaryEndpoint});
 
     var _this = this;
     return companyModel.fetchCurrent({success: function(model, response, options) {
